4c71c381caa51fb15cebaef3aae7803199083f95
unknown <95309102+TheObtey@users.noreply.github.com>
2025-05-21 14:26:36 +0200

    Fix typo errors

--- lua/tomysqlite/core/base_driver.lua
+++ lua/tomysqlite/core/base_driver.lua
@@ -7,7 +7,7 @@ BaseDriver = {}
 BaseDriver.__index = BaseDriver
 
 /*
-	Create a new instance of the BaseDriver class.
+	Creates a new instance of the BaseDriver class.
 
 	@return self (BaseDriver) - A new instance of BaseDriver
 */
@@ -17,7 +17,7 @@ function BaseDriver:new()
 end
 
 /*
-	Establishe a connection to the database.
+	Establishes a connection to the database.
 	This method must be overridden by the driver
 */
 function BaseDriver:connect()
@@ -25,18 +25,18 @@ function BaseDriver:connect()
 end
 
 /*
-	Execute a query.
+	Executes a query.
 
 	@param queryString (string) - The SQL query to execute
-	@param params (table) - The parameters for the query (optionnal)
-	@param callback (function) - The function to call with the result (optionnal)
+	@param params (table) - The parameters for the query (optional)
+	@param callback (function) - The function to call with the result (optional)
 */
 function BaseDriver:query(queryString, params, callback)
 	error("[TOMYSQLITE] query() is not implemented.")
 end
 
 /*
-	Execute a query and return a table of results.
+	Executes a query and return a table of results.
 
 	@param queryString (string) - The SQL query to execute
 	@param params (table) - The parameters for the query (optionnal)
@@ -47,7 +47,7 @@ function BaseDriver:queryAssoc(queryString, params, callback)
 end
 
 /*
-	Escape a value to be safely used in a query.
+	Escapes a value to be safely used in a query.
 
 	@param value (string|number) - The value to escape
 	@param escapedValue (string) - The escapedValue
